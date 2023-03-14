0eb839ec0ff28a43b0fc209988a55c363b3755a4
yungerre <124702518+yungerre@users.noreply.github.com>
2023-03-13 21:33:40 -0400

    Update template-processor.js

--- project2/template-processor.js
+++ project2/template-processor.js
@@ -4,6 +4,16 @@ function TemplateProcessor(template) {
 }
 
 TemplateProcessor.prototype.fillIn = function(dictionary) {
-    
-    return this.template;
+    var res = this.template;
+    var re = /{{[^{]*}}/g;
+    var match = this.template.match(re);
+    var pre, key, after;
+    for (var i = 0; i < match.length; i++) {
+        pre = match[i];
+        key = pre.replace("{{", "");
+        key = key.replace("}}", "");
+        after = dictionary[key] ||'';
+        res = res.replace(pre, after);
+    }
+    return res;
 }
